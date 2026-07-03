a16944d7404a30213e51f2bbfac4923d346134b9
zanebennion <zanebennion@gmail.com>
2026-07-02 20:27:07 -0800

    Added color constants

---
+++ lib/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color bg = Color(0xffF5F5F5);
+  static const Color bgAccent = Color(0xffD8D8D9);
+  static const Color text = Color(0xff555358);
+  static const Color blue = Color(0xff1B98E0);
+  static const Color green = Color(0xff8DAA91);
+  static const Color purple = Color(0xffDEC1FF);
+  static const Color pink = Color(0xffD6C3C9);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,53 @@
+import 'package:budget_app/colors.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.pinkAccent,
-          title: const Text('Dead Simple Budget'),
-        ),
-        body: Container(child: const Text('hello there :)')),
+    return const MaterialApp(title: 'Budget App', home: BudgetScreen());
+  }
+}
+
+class BudgetScreen extends StatefulWidget {
+  const BudgetScreen({super.key});
+
+  @override
+  State<BudgetScreen> createState() => _BudgetScreenState();
+}
+
+class _BudgetScreenState extends State<BudgetScreen> {
+  int budget = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Budget App')),
+      body: Column(
+        children: [
+          BudgetDisplay(budget: budget,),
+          Expanded(child: Keypad()),
+        ],
       ),
+    );
+  }
+}
+
+class BudgetDisplay extends StatelessWidget {
+  final int budget;
+
+  const BudgetDisplay({super.key, required this.budget})
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      color: AppColors.blue,
     );
   }
 }
